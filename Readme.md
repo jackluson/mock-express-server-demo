6e5c6539cde0de055f22936d49e4e9a049fab5a6
camel-lu <36684351+jackluson@users.noreply.github.com>
2021-08-14 20:21:14 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# 介绍
+
+### 启动
+
+> 启动 `start` 命令
+
+### 请求
+
+在 demo 浏览器输入`https://526ri.sse.codesandbox.io/v2/v1/active-user/delivery/status` 触发 get 请求即可看到返回结果
---
+++ mock.config.js
@@ -0,0 +1,18 @@
+const config = {
+  port: 8080, // 启动端口
+  localPath: "local/", // 1. 本机绝对路径；2. 相对项目根目录路径（拼接`process.cwd()`）路径；（若是文件夹路径，则会遍历该文件夹下的所有文件 swagger 配置文件), 例如：'local/api-docs.json'
+  tag: "", // 对应swagger config 的tags，空的话,则选择全部tags的path， 配置的话经过筛选后,只启动该tag下面的接口, 例如：/pet'
+  url: "https://petstore.swagger.io/v2/swagger.json", // swagger config 接口路径,例如：https://petstore.swagger.io/v2/swagger.json, 如果同时配置了url与localPath，合并两者，若有冲突，以url配置为止
+  openLocalRedis: false, // 是否开始redis存储swagger 配置（一般用于开发阶段）
+  openValidParams: false, // 是否检验请求参数类型合法，false，则不检验, 直接返回响应数据
+  // 请求配置
+  // 响应配置
+  isHttps: true,
+  codeMap: {
+    success: 20000, // 成功逻辑code
+    unlogin: 40001, // 没有登录
+    parameterError: 40003 //参数错误
+  }
+};
+
+module.exports = config;
